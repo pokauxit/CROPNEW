bfd4eceb69cd0255ca60137dc33acb1d79d82131
Test <apdev.juy@gmail.com>
2016-05-27 22:47:37 +0700

    Add Modules Ageiculturist
    เพิ่มโมดูล Agriculturist

---
+++ App/Modules/Ageiculturist/Controllers/Ageiculturist.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Modules\Ageiculturist\Controllers;
+use System\HMVC\HMVC;
+use App\Models\ageiculturist as agl;
+use App\Models\ACL;
+
+class Ageiculturist extends HMVC{
+    
+     protected  $db;
+     protected  $row;
+     
+     public function __construct() {
+         ACL::check("STAFF");
+         parent::__construct();
+     }
+
+     public function index() {
+        $this->db = new agl();
+        $this->db->select();
+        $this->view();
+     }
+     
+    public function  Add(){
+        if(SUBMIT){
+            $this->controller();
+        }else{
+            $this->view("Add");
+        }
+        
+    }
+    
+    public function  Edit(){
+         $this->db = new agl();
+        if(SUBMIT){
+            $this->controller();
+        }else{
+            $this->row = $this->db->get(ID);
+            $this->view("Edit");
+        }
+        
+    }
+    
+    public function Delete(){
+        $this->controller();
+    }
+}    
+?>
---
+++ App/Modules/Ageiculturist/Views/Ageiculturist.php
@@ -0,0 +1,5 @@
+<?php
+    while($rc = $this->db->fetch()){
+    print_r($rc);
+}
+?>
