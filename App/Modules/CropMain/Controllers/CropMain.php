786a86f3e6119ef50e699df212b9ecf5d21e08cd
pokauxit <lertsak.hongjan@gmail.com>
2016-05-28 14:23:52 +0700

    CropMian

---
+++ App/Modules/CropMain/Controllers/CropMain.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Modules\CropMain\Controllers;
+use System\HMVC\HMVC;
+
+class CropMain extends HMVC{
+    public function index() {
+        $this->view();
+    }
+
+}
--- System/Template/Template.php
+++ System/Template/Template.php
@@ -132,7 +132,11 @@ public function close(){
     $(document).ready(function () {
         // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
         $('.modal-trigger').leanModal();
-        $('select').material_select();
+        $('select').material_select();.
+ 
+    $('ul.tabs').tabs();
+ 
+        
 
     });
 </script>
