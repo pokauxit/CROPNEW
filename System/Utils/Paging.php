d7cba07075d0ee74fadefae81fdd3beec79942c5
NamwanSoftTH <anusit_khuandee@hotmail.com>
2016-06-11 20:03:37 +0700

    paging lng TH

--- System/Utils/Paging.php
+++ System/Utils/Paging.php
@@ -25,7 +25,6 @@ class Paging {
     
     $html .= '<style>.pagination li{padding: 0px;}.pagination li .padding-10{padding: 0 10px;}.pagination li .padding-5{padding: 0 5px;}</style>';
     
-    //¨Ó¹Ç¹àÅ¢Ë¹éÒ·ÕèµéÍ§¡ÒÃáÊ´§
     $showpages = "7"; //1,3,5,7,9...
 
     $pages = ceil($total / $limit);
@@ -34,13 +33,12 @@ class Paging {
 
     $html .= '<ul class="pagination">';
     $previous = $pagenumber - 1;
-    //Ë¹éÒ»Ñ¨¨ØºÑ¹ÁÒ¡¡ÇèÒ 1 ãËéãªé§Ò¹ä´é
     if ($pagenumber > 1){
-        $html .= '<li class=""><a class="padding-5" href="'.$baseurl.'1" title="Ë¹éÒáÃ¡"><i class="fa fa-angle-double-left"></i></a></li>';
-        $html .= '<li class=""><a class="padding-5" href="'.$baseurl.$previous.'" title="ÂéÍ¹¡ÅÑº"><i class="fa fa-angle-left"></i></a></li>';
+        $html .= '<li class=""><a class="padding-5" href="'.$baseurl.'1" title="หน้าแรก"><i class="fa fa-angle-double-left"></i></a></li>';
+        $html .= '<li class=""><a class="padding-5" href="'.$baseurl.$previous.'" title="ถัดไป"><i class="fa fa-angle-left"></i></a></li>';
     }else{
-        $html .= '<li class="disabled"><a class="padding-5" title="Ë¹éÒáÃ¡"><i class="fa fa-angle-double-left"></i></a></li>';
-        $html .= '<li class="disabled"><a class="padding-5" title="ÂéÍ¹¡ÅÑº"><i class="fa fa-angle-left"></i></a></li>';
+        $html .= '<li class="disabled"><a class="padding-5" title="หน้าแรก"><i class="fa fa-angle-double-left"></i></a></li>';
+        $html .= '<li class="disabled"><a class="padding-5" title="ถัดไป"><i class="fa fa-angle-left"></i></a></li>';
     }
     if ($pages >= 2) {
         $p = 1;
@@ -65,9 +63,9 @@ class Paging {
         while ($p <= $pages) {
             if ($p > $minpage && $p < $maxpage) {
                 if ($pagenumber == $p) {
-                    $html .= '<li class="active"><a class="padding-10" title="Ë¹éÒ '.$p.'">'.$p.'</a></li>';
+                    $html .= '<li class="active"><a class="padding-10" title="หน้า '.$p.'">'.$p.'</a></li>';
                 } else {
-                    $html .= '<li class=""><a class="padding-10" href="'.$baseurl.$p.'" title="Ë¹éÒ '.$p.'">'.$p.'</a></li>';
+                    $html .= '<li class=""><a class="padding-10" href="'.$baseurl.$p.'" title="หน้า '.$p.'">'.$p.'</a></li>';
                 }
             }
             $p++;
@@ -78,13 +76,12 @@ class Paging {
     }
 
     $next = $pagenumber + 1;
-    //Ë¹éÒ»Ñ¨¨ØºÑ¹¹éÍÂ¡ÇèÒË¹éÒÊØ´·éÒÂãËéãªé§Ò¹ä´é
     if ($pagenumber < $pages){
-        $html .= '<li class=""><a class="padding-5" href="'.$baseurl.$next.'" title="¶Ñ´ä»"><i class="fa fa-angle-right"></i></a></li>';
-        $html .= '<li class=""><a class="padding-5" href="'.$baseurl.$pages.'" title="Ë¹éÒÊØ´·éÒÂ"><i class="fa fa-angle-double-right"></i></a></li>';
+        $html .= '<li class=""><a class="padding-5" href="'.$baseurl.$next.'" title="ย้อนกลับ"><i class="fa fa-angle-right"></i></a></li>';
+        $html .= '<li class=""><a class="padding-5" href="'.$baseurl.$pages.'" title="หน้าสุดท้าย"><i class="fa fa-angle-double-right"></i></a></li>';
     }else{
-        $html .= '<li class="disabled"><a class="padding-5" title="¶Ñ´ä»"><i class="fa fa-angle-right"></i></a></li>';
-        $html .= '<li class="disabled"><a class="padding-5" title="Ë¹éÒÊØ´·éÒÂ"><i class="fa fa-angle-double-right"></i></a></li>';
+        $html .= '<li class="disabled"><a class="padding-5" title="ย้อนกลับ"><i class="fa fa-angle-right"></i></a></li>';
+        $html .= '<li class="disabled"><a class="padding-5" title="หน้าสุดท้าย"><i class="fa fa-angle-double-right"></i></a></li>';
     }
     $html .= '</ul>';
     return $html;
