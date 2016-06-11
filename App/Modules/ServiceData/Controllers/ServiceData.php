28f1055a6624537bcb9536d4539fd1de39004d8f
NamwanSoftTH <anusit_khuandee@hotmail.com>
2016-06-11 22:17:02 +0700

    Multi Join Service Data

--- App/Modules/ServiceData/Controllers/ServiceData.php
+++ App/Modules/ServiceData/Controllers/ServiceData.php
@@ -1,53 +1,67 @@
 <?php
+
 namespace App\Modules\ServiceData\Controllers;
+
 use System\HMVC\HMVC;
 use App\Models\crop;
 use App\Models\plant as pln;
 use App\Models\ageiculturist as agl;
+use App\Models\tambon;
+use App\Models\amphur;
+use App\Models\province;
 
-class ServiceData extends HMVC{
-    
-    protected $dbPlant; 
-        protected $db; 
-        
-        
+class ServiceData extends HMVC {
+
+    protected $dbPlant;
+    protected $db;
+
     public function index() {
         
     }
-    
-    public function getCropByID($id){
-        
-           $tb = new crop();
-          return $tb->get($id);
-         
+
+    public function getCropByID($id) {
+
+        $tb = new crop();
+        return $tb->get($id);
     }
-    public function showAgeiculturist($id){
-         
-         $this->db = new agl();
-          $this->db->where =$this->db->pk()."=".$id;
-        $this->db->left("tambon_id", "tambon.tambon_name");
-          $this->view("Ageiculturist");
+
+    public function showAgeiculturist($id) {
+        $this->db = new agl();
+
+        $tumbon = new tambon();
+        $tumbon->display = "tambon_name";
+        $tumbon->fk = "amphur_id";
+
+        $amphur = new amphur();
+        $amphur->fk = "province_id";
+        $amphur->display = "amphur_name";
+        $amphur->moreDisplay = "postcode";
+
+        $province = new province();
+        $province->display = "province_name";
+
+        $this->db->where = $this->db->pk() . "=" . $id;
+        $this->db->multiLeftJoin("tambon_id", array($tumbon, $amphur, $province));
+
+        $this->view("Ageiculturist");
     }
-    
-    public function getAgeiculturist($id){
-        
-         $this->db = new agl(); 
-          return $this->db->get($id);
-       
+
+    public function getAgeiculturist($id) {
+
+        $this->db = new agl();
+        return $this->db->get($id);
     }
-    
-    public function showPlant($id){
-       
-       $this->dbPlant = new pln();
-       $this->dbPlant->where =$this->dbPlant->pk()."=".$id;
-        $this->dbPlant->left("type_id", "typeplant.type_name"); 
+
+    public function showPlant($id) {
+        $this->dbPlant = new pln();
+        $this->dbPlant->where = $this->dbPlant->pk() . "=" . $id;
+        $this->dbPlant->left("type_id", "typeplant.type_name");
         $this->view("Plant");
     }
-    
-    public function getPlant($id){
-        
-         $this->db = new pln(); 
-          return $this->db->get($id);
-       
+
+    public function getPlant($id) {
+        $this->db = new pln();
+        return $this->db->get($id);
     }
+
 }
--- App/Modules/ServiceData/Views/Ageiculturist.php
+++ App/Modules/ServiceData/Views/Ageiculturist.php
@@ -1,28 +1,25 @@
 <div class="row  " style="padding: 10px;"> 
-   <?php  $rc = $this->db->fetch();?>
-     <div class=" col s12 left"> 
-            <b >ข้อมูลเกษตรกร  </b>
-            
-         
-        </div>    
+    <?php $rc = $this->db->fetch(); ?>
+    <div class=" col s12 left"> 
+        <b >ข้อมูลเกษตรกร  </b>
+
+
+    </div>    
     <div class=" col s12 m6 "> 
-            <label >เกษตร : </label>
-            
-         <?php echo  $rc->agriculturist_name;?>
-        </div>
-    <div class=" col s12 m6 "> 
-            <label >บ้านเลขที่ : </label>
-            
-         <?php echo  $rc->home_no;?>
-        </div>
-    <div class=" col s12 m6 "> 
-            <label >ตำบล : </label>
-            
-         <?php echo  $rc->tambon_tambon_name;?>
-        </div>
-    <div class=" col s12 m6 "> 
-            <label >หมายเลขโทรศัพท์ : </label>
-            
-         <?php echo  $rc->phone_no;?>
-        </div>
+        <label >เกษตร : </label>
+
+        <?php echo $rc->agriculturist_name; ?>
     </div>
+    <div class=" col s12 m6 "> 
+        <label >บ้านเลขที่ : </label>
+        <?php echo $rc->home_no; ?>
+        <label >ตำบล : </label>
+        <?php echo $rc->tambon_tambon_name; ?>
+        <label >จังหวัด : </label>
+        <?php echo $rc->province_province_name; ?> <?php echo $rc->postcode; ?>
+    </div>
+    <div class=" col s12 m6 "> 
+        <label >หมายเลขโทรศัพท์ : </label>
+        <?php echo $rc->phone_no; ?>
+    </div>
+</div>
