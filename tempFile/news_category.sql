f42310ce4e411442ca9e0773a9abec61eb1605a4
Test <apdev.juy@gmail.com>
2016-06-20 21:49:27 +0700

    ตารางข่าว

---
+++ tempFile/news_category.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 20, 2016 at 09:47 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news_category`
+--
+
+CREATE TABLE `news_category` (
+  `news_category_id` int(10) NOT NULL COMMENT 'รหัสประเภทข่าว',
+  `news_category_name` varchar(255) NOT NULL COMMENT 'ชื่อประเภทข่าว'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `news_category`
+--
+ALTER TABLE `news_category`
+  ADD PRIMARY KEY (`news_category_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `news_category`
+--
+ALTER TABLE `news_category`
+  MODIFY `news_category_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'รหัสประเภทข่าว';
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
