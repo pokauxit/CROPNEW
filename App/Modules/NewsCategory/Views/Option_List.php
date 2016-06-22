cb8e7541a4903faf228d64317a520c8c7a324a94
pokauxit <lertsak.hongjan@gmail.com>
2016-06-22 19:57:21 +0700

    ลบออกจาก CROP

--- App/Modules/NewsCategory/Views/Option_List.php
+++
@@ -1,4 +0,0 @@
-<option disabled selected>กรุณาเลือกรายการ</option>
-<?php while ($rc = $this->db->fetch()) { ?>
-    <option value="<?php echo $rc->news_category_id; ?>" <?php if($this->id==$rc->news_category_id){echo 'selected';} ?>><?php echo $rc->news_category_name; ?></option>
-<?php } ?>
